86fa50ff647f68958b1086a8119baf18e97e6798
Brenda Jow <brendaj0214@gmail.com>
2023-02-22 16:50:28 -0800

    acp

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 'use strict';
 
 require('dotenv').config();
-const app = require('./src/server.js');
-const { db } = require('./src/auth/models');
+const app = require('./newsrc/server.js');
+const { db } = require('./newsrc/auth/models');
 
 db.sync().then(() => {
   app.start(process.env.PORT || 3001);
